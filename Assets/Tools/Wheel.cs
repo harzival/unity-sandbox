60609fb85d422066641eef73435991b1d1ea6722
SyntleLmao <syntlelmao@gmail.com>
2019-01-31 20:18:25 +0000

    Basic wheel spawning tool

---
+++ Assets/Tools/Wheel.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Wheel : MonoBehaviour
+{
+    private GameObject Camera;
+    public GameObject wheelPrefab;
+    private Vector3 SpawnPosition;
+    private int DistanceToCamera = 5;
+
+    void Start () {
+         Camera = (GameObject) GameObject.FindWithTag("MainCamera");
+     }
+
+    private void Update() {
+        if (Input.GetKeyDown("o")) {
+            SpawnPosition = Camera.transform.forward * DistanceToCamera + Camera.transform.position;
+            Instantiate(wheelPrefab, SpawnPosition, Quaternion.identity);
+        }
+    }
+
+}
