68079e83be686ffb68a5a3f8d939ef5eb388b530
BuildTools <unconfigured@null.spigotmc.org>
2019-02-07 01:43:24 +0000

    -Added runtime assetbundle loader for user object management. Assetbundles can now be loaded from the StreamingAsset/Objects folder and stored in the tiles in the object list. -Added tool gun model to the players arm, still needs a laser beam and controller etc.

--- Assets/Scripts/ObjectListController.cs
+++ Assets/Scripts/ObjectListController.cs
@@ -1,6 +1,66 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.IO;
+using TMPro;
+
+public class ObjectListController : MonoBehaviour
+{
+    public RectTransform listItemPrefab;
+    private RectTransform contentRectTransform;
+    private float listPosX = 14.25F;
+    private float listPosY = -14.25F;
+
+    void Start ()
+    {
+        contentRectTransform = this.GetComponent<RectTransform> ();
+
+        // Define the path to the "Objects" mod folder.
+        string objectsDir = Path.Combine ( Application.streamingAssetsPath + "/Objects" );
+        // Get a list of AssetBundles in the "Objects" mod folder.
+        string [] objectsDirFileList = Directory.GetFiles ( objectsDir, "*.assetbundle" );
+
+        int objectsCounter = 0;
+        for ( int i = 0; i < 10; i++ )
+        {
+            for ( int j = 0; j < 6; j++ )
+            {
+                if ( objectsCounter != objectsDirFileList.Length )
+                {
+                    RectTransform currentTile = Instantiate ( listItemPrefab, contentRectTransform );
+                    GameObject objectPrefab = LoadObjectFromAssetBundle ( objectsDirFileList [ objectsCounter ] );
+                    currentTile.GetComponent<ListItemController> ().objectPrefab = objectPrefab;
+                    currentTile.anchoredPosition = new Vector2 ( listPosX, listPosY );
+                    currentTile.GetComponentInChildren<TextMeshProUGUI> ().SetText(objectPrefab.name);
+                    objectsCounter++;
+                    listPosX += 4.25F + 20;
+                }
+                else
+                {
+                    break;
+                }
+            }
+            listPosY += -7.25F - 20;
+            listPosX = 14.25F;
+        }
+
+    }
+    GameObject LoadObjectFromAssetBundle ( string objectFilePath )
+    {
+        AssetBundle objectAssetBundle = AssetBundle.LoadFromFile ( objectFilePath );
+        return objectAssetBundle.LoadAsset<GameObject> ( "Sphere" ); 
+    }
+    void PopulateObjectListGrid ()
+    {
+
+    }
+}
+
+
+
+/*using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
 
 public class ObjectListController : MonoBehaviour
 {
@@ -29,3 +89,4 @@ public class ObjectListController : MonoBehaviour
 
     }
 }
+*/
