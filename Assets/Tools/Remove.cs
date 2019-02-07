3963ed0dfc21ee7ecba57d5ecd128af230eb86a7
Syntle <syntlelmao@gmail.com>
2019-02-07 13:17:48 +0000

    Added Remove tool with basic functionality

---
+++ Assets/Tools/Remove.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+public class Remove : MonoBehaviour
+{
+    public float Range = 100f;
+
+    public Camera playerCam;
+
+    void Update()
+    {
+        if(Input.GetButtonDown("Fire1")){
+            Shoot();
+        }
+    }
+
+    void Shoot(){
+        RaycastHit hit;
+
+        if (Physics.Raycast(playerCam.transform.position, playerCam.transform.forward, out hit, Range)){
+            Destroy(hit.transform.gameObject);
+        }
+    }
+
+}
